ad70e12337d4ea5852db620e6e22945190df5144
Abdallah Mahameed <a.mahameed2000@gmail.com>
2023-12-30 18:55:43 +0100

    Initial Commit

---
+++ Facts App/Facts_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facts_AppApp.swift
+//  Facts App
+//
+//  Created by Abdallah Mahameed on 30/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Facts_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
